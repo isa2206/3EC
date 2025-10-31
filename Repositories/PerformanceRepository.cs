70468aabfe7131c66f1adc43b98782386f1896e5
isa2206 <hidalgoavilaisabel@gmail.com>
2025-10-31 10:46:48 -0400

    feat: Inyecctions

--- Repositories/PerformanceRepository.cs
+++ Repositories/PerformanceRepository.cs
@@ -7,7 +7,11 @@ namespace TecWebFest.Repositories
     public class PerformanceRepository : IPerformanceRepository
     {
         // TODO INYECCION 
-
+        private readonly AppDbContext _ctx;
+        public PerformanceRepository(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
         public async Task AddAsync(Performance performance)
         {
             await _ctx.Performances.AddAsync(performance);
--- Repositories/StageRepository.cs
+++ Repositories/StageRepository.cs
@@ -6,7 +6,11 @@ namespace TecWebFest.Repositories
     public class StageRepository : IStageRepository
     {
         // TODO INYECCION 
-
+        private readonly AppDbContext _ctx;
+        public StageRepository(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
         public Task<bool> ExistsAsync(int id) =>
             _ctx.Stages.AnyAsync(s => s.Id == id);
     }
