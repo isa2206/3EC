9200a31f051f6ba2a6fa9bebda2eda780a54b2de
isa2206 <hidalgoavilaisabel@gmail.com>
2025-10-31 09:25:27 -0400

    feat: 1.Relations

--- Models/Festival.cs
+++ Models/Festival.cs
@@ -10,5 +10,7 @@ namespace TecWebFest.Api.Entities
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         //TODO relacion con stage
+        public ICollection<Stage> Stages { get; set; } = new List<Stage>();
+
     }
 }
